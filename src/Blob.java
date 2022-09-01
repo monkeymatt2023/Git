0d2735b0716d97bc5921050c809da5f3e0a150fd
benjiham <bham1@hwemail.com>
2022-09-01 15:02:11 -0700

    Created SHA 1 hex from file
    sha1 file works i think

--- src/Blob.java
+++ src/Blob.java
@@ -1,4 +1,44 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class Blob {
 
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+        Blob fileHash = new Blob();
+        
+        //Use filepath not the filename
+        System.out.println(fileHash.sha1Code("testfile.txt"));
+
+    }
+
+
+    public String sha1Code(String filePath) throws IOException, NoSuchAlgorithmException {
+        FileInputStream fileInputStream = new FileInputStream(filePath);
+        MessageDigest digest = MessageDigest.getInstance("SHA-1");
+        DigestInputStream digestInputStream = new DigestInputStream(fileInputStream, digest);
+        byte[] bytes = new byte[1024];
+        // read all file content
+        while (digestInputStream.read(bytes) > 0);
+
+//        digest = digestInputStream.getMessageDigest();
+        byte[] resultByteArry = digest.digest();
+        return bytesToHexString(resultByteArry);
+    }
+    
+    public static String bytesToHexString(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (byte b : bytes) {
+            int value = b & 0xFF;
+            if (value < 16) {
+                // if value less than 16, then it's hex String will be only
+                // one character, so we need to append a character of '0'
+                sb.append("0");
+            }
+            sb.append(Integer.toHexString(value).toUpperCase());
+        }
+        return sb.toString();
+    }
 }
