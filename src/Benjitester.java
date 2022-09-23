8a61c091bfc469d5a1d7aa81f54b662710fb63ab
Matthew Chang <monkeymatt2023@gmail.com>
2022-09-23 16:53:52 -0700

    Tester Stuff

--- src/Benjitester.java
+++ src/Benjitester.java
@@ -14,7 +14,10 @@ import org.junit.jupiter.api.Test;
 class Benjitester {
 	@BeforeAll
 	static void setUpBeforeClass() throws IOException{
-        Path p = Paths.get("junit.txt");
+		File f = new File("Test/objects");
+		f.mkdir();
+		
+		Path p = Paths.get("Test/junit.txt");
         try {
             Files.writeString(p, "Will Tester", StandardCharsets.ISO_8859_1);
         } catch (IOException e) {
@@ -24,8 +27,28 @@ class Benjitester {
 	@AfterAll
 	
 	static void tearDownAfterClass() throws IOException{
-		File f = new File("junit.txt");
-		f.delete();
+		File f1 = new File("junit.txt");
+		f1.delete();
+		File f1hash = new File("Test/objects/b34bcfc4d490f93ed9cf4e560c608b58cd688c24");
+		f1hash.delete();
+		
+		File f2 = new File("Test/objects/dd4840f48a74c1f97437b515101c66834b59b1be");
+		f2.delete();
+		
+		File f3 = new File("Test/index.txt");
+		f3.delete();
+		
+		File f4 = new File("Test/objects");
+		deleteDir(f4);
+	}
+	
+	static void deleteDir(File file) {
+	    File[] contents = file.listFiles();
+	    if (contents != null) {
+	        for (File f : contents)
+	            deleteDir(f);
+	    }
+	    file.delete();
 	}
 
 	@Test
@@ -33,18 +56,18 @@ class Benjitester {
 		Index g = new Index();
 		g.initialize();
 		
-		File file = new File("index.txt");
+		File file = new File("Test/index.txt");
 		assertTrue(file.exists());
 		
-		Path path  = Paths.get("objects");
+		Path path = Paths.get("Test/objects");
 		assertTrue(Files.exists(path));
 		
 	}
 	
 	@Test
 	void testBlob() throws NoSuchAlgorithmException, IOException {
-		Blob b = new Blob("junit.txt");
-		File file  = new File("objects/b34bcfc4d490f93ed9cf4e560c608b58cd688c24");
+		Blob b = new Blob("Test/junit.txt");
+		File file  = new File("Test/objects/b34bcfc4d490f93ed9cf4e560c608b58cd688c24");
 		assertTrue(file.exists());
 	}
 	
@@ -52,7 +75,7 @@ class Benjitester {
 	void testAdd() throws IOException, NoSuchAlgorithmException {
 		Index i = new Index();
 		i.add("junit.txt");
-		File file = new File("objects/b34bcfc4d490f93ed9cf4e560c608b58cd688c24");
+		File file = new File("Test/objects/b34bcfc4d490f93ed9cf4e560c608b58cd688c24");
 		assertTrue(file.exists());
 	}
 	
@@ -60,8 +83,8 @@ class Benjitester {
 	@Test
 	void testRemove() throws NoSuchAlgorithmException, IOException {
 		Index i = new Index();
-		i.remove("junit.txt");
-		File file = new File("objects/b34bcfc4d490f93ed9cf4e560c608b58cd688c24");
+		i.remove("Test/junit.txt");
+		File file = new File("Test/objects/b34bcfc4d490f93ed9cf4e560c608b58cd688c24");
 		assertTrue(file.exists());
 	}
 	
@@ -73,17 +96,15 @@ class Benjitester {
 		listy.add("blob : f1d82236ab908c86ed095023b1d2e6ddf78a6d83");
 		listy.add("tree : bd1ccec139dead5ee0d8c3a0499b42a7d43ac44b");
 		listy.add("tree : e7d79898d3342fd15daf6ec36f4cb095b52fd976");
+		File file = new File("Test/objects/dd4840f48a74c1f97437b515101c66834b59b1be");
 		Tree tree = new Tree(listy);
-		File file = new File("objects/dd4840f48a74c1f97437b515101c66834b59b1be");
-		assertTrue(file.exists());
+		//assertTrue(file.exists());
 		Scanner s = new Scanner(file);
 		assertTrue(s.nextLine().equals("blob : 81e0268c84067377a0a1fdfb5cc996c93f6dcf9f"));
 		assertTrue(s.nextLine().equals("blob : 01d82591292494afd1602d175e165f94992f6f5f"));
 		assertTrue(s.nextLine().equals("blob : f1d82236ab908c86ed095023b1d2e6ddf78a6d83"));
 		assertTrue(s.nextLine().equals("tree : bd1ccec139dead5ee0d8c3a0499b42a7d43ac44b"));
 		assertTrue(s.nextLine().equals("tree : e7d79898d3342fd15daf6ec36f4cb095b52fd976"));
-		file.delete();
-
 	}
 
 }
